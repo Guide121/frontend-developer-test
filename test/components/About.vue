164817d16705603340a4acf850790f206678cc16
Débora Barros <debora_lbarros@outlook.com>
2021-02-15 16:11:17 -0400

    feat: create about, button, features, header and info components

---
+++ test/components/About.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="main-container about-container">
+    <div class="info">
+      <h1>Want anything to be easy with <b>LaslesVPN.</b></h1>
+
+      <p>
+        Provide a network for all your needs with ease and fun using
+        <b>LaslesVPN</b>
+        discover interesting features from us.
+      </p>
+
+      <Button title="Get Start" primary />
+    </div>
+
+    <img src="@/assets/about.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    printConsole() {
+      console.log('a')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.about-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 70vh;
+}
+
+.info {
+  width: 50%;
+  height: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-start;
+}
+
+h1 {
+  color: #0b132a;
+  font-weight: 500;
+  width: 70%;
+  font-size: 2.4rem;
+}
+
+h1 b,
+p b {
+  font-weight: 700;
+}
+
+p {
+  color: #4f5665;
+  width: 90%;
+  /* margin: 20px 0; */
+}
+</style>
---
+++ test/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="header-container">
+    <img src="@/assets/logo.png" />
+
+    <nav>
+      <NuxtLink to="#">About</NuxtLink>
+      <NuxtLink to="#">Features</NuxtLink>
+      <NuxtLink to="#">Pricing</NuxtLink>
+      <NuxtLink to="#">Testimonials</NuxtLink>
+      <NuxtLink to="#">Help</NuxtLink>
+    </nav>
+
+    <div>
+      <button class="buttons sign-in">Sign In</button>
+      <button class="buttons sign-up">Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.header-container {
+  width: 93%;
+  margin: 0 auto 40px;
+  padding: 25px;
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+}
+
+nav {
+  width: 50%;
+  display: flex;
+  padding: 0 30px;
+  justify-content: space-around;
+}
+
+nav a {
+  color: #4f5665;
+  text-decoration: none;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+
+.buttons {
+  background: transparent;
+  font-family: Rubik, sans-serif;
+  font-weight: 500;
+  width: 150px;
+  padding: 10px;
+  outline: none;
+  cursor: pointer;
+}
+
+.sign-in {
+  color: #0b132a;
+  border: none;
+}
+
+.sign-up {
+  color: #f53855;
+  border: 1px solid #f53855;
+  border-radius: 20px;
+}
+</style>
