2ab5d3d2cbe0e1a975a4a4b270a2f5f7e220ae15
Débora Barros <debora_lbarros@outlook.com>
2021-02-15 13:55:06 -0400

    initial commit

--- test/nuxt.config.js
+++ test/nuxt.config.js
@@ -32,7 +32,13 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
