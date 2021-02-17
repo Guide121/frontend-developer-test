d3fc48e76ef6841de4d0d138ff5e359cf3f7e255
Débora Barros <debora_lbarros@outlook.com>
2021-02-17 16:20:06 -0400

    feat: create help component and finish all basic components

--- test/components/Help.vue
+++ test/components/Help.vue
@@ -22,6 +22,34 @@
 
         <p class="copy">&copy;2020Lasles<b>VPN</b></p>
       </div>
+
+      <div class="links">
+        <ul>
+          <li class="title">Product</li>
+          <li>Download</li>
+          <li>Pricing</li>
+          <li>Locations</li>
+          <li>Server</li>
+          <li>Countries</li>
+          <li>Blog</li>
+        </ul>
+
+        <ul>
+          <li class="title">Engage</li>
+          <li>LaslesVPN ?</li>
+          <li>FAQ</li>
+          <li>Tutorials</li>
+          <li>About Us</li>
+          <li>Privacy Policy</li>
+          <li>Terms of Service</li>
+        </ul>
+
+        <ul>
+          <li class="title">Earn Money</li>
+          <li>Affiliate</li>
+          <li>Become Partner</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -37,22 +65,22 @@ export default {}
   z-index: 1;
   height: auto;
   background: #f8f8f8;
-
-  display: flex;
 }
 
 .container {
   padding-top: 150px;
+  display: flex;
 }
 
 .text {
   margin: 10px 0;
   color: #4f5665;
-  width: 30%;
+  width: 55%;
 }
 
 .social-medias {
   width: 20%;
+  display: inline;
 }
 
 .social-medias button {
@@ -68,14 +96,23 @@ export default {}
 
 .links {
   display: flex;
-}
-
-.title {
-  color: #0b132a;
+  width: 50%;
+  justify-content: space-between;
 }
 
 ul {
   list-style: none;
   padding-left: 0;
+}
+
+.title {
+  color: #0b132a;
+  font-weight: 500;
+}
+
+li {
+  color: #4f5665;
+  margin: 15px 0;
+  cursor: pointer;
 }
 </style>
