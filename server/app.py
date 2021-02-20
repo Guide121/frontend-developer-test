cd233f9eec12e50c02c898c251e52836f4631df8
Débora Barros <debora_lbarros@outlook.com>
2021-02-20 17:17:17 -0400

    chore: add cors dependecie

--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 from main import evaluations, create_evaluations
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 filename = 'evaluations.csv'
 
